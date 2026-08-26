75e9cd639bf4aab2e0ab4d9098fc0fb2c221ab63
GrigoriyVolk <grigoriyvolk228@gmail.com>
2026-08-26 12:52:24 +0300

    добавил работу с minio

---
+++ src/main/java/com/example/services/MinioService.java
@@ -0,0 +1,30 @@
+package com.example.services;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@RequiredArgsConstructor
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket}")
+    private String bucket;
+
+    public void upload(MultipartFile file, String fileName) throws Exception {
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(fileName)
+                        .stream(file.getInputStream(), file.getSize(), 10485760L)
+                        .contentType(file.getContentType())
+                        .build()
+        );
+    }
+
+}
